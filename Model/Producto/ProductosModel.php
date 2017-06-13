5fe86a5b900f4bfda88ef28073e14d23ad674c55
JGuajan <betos_1416@yahoo.com>
2017-06-13 10:19:55 -0500

    agregue scrip inventario

--- Model/Producto/ProductosModel.php
+++ Model/Producto/ProductosModel.php
@@ -1,5 +1,19 @@
 <?php
+include_once '../DataBase.php';
+include_once 'Producto.php';
 
 class ProductosModel {
-    //put your code here
+    public function getProducto() {
+        // Obtención de informacion de la Base de Datos mediante consulta sql
+        $pdo = Database::connect();
+        $sql = "select * from inv_tab_productos order by ID_PROD";
+        $resultado = $pdo->query($sql);
+
+        //transformamos los registros en objetos de tipo Producto y guardamos en array
+        $listadoProductos = array();
+        foreach ($resultado as $res) {
+            $producto = new Producto($res['ID_PROD'], $res['NOMBRE_PROD'], $res['DESCRIPCION_PROD'], $res['NOMBRES_USU'], $res['APELLIDOS_USU'], $res['FECH_NAC_USU'], $res['CIUDAD_NAC_USU'], $res['DIRECCION_USU'], $res['FONO_USU'], $res['E_MAIL_USU'], $res['ESTADO_USU']);
+            array_push($listadoUsuarios, $usuario);
+        }
+    }
 }
