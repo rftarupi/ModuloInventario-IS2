bf9551fd1adc72d0072525d18d11302cb1947fcb
JGuajan <betos_1416@yahoo.com>
2017-06-14 10:36:06 -0500

    Crud Producto

--- View/Producto/inicioProductos.php
+++ View/Producto/inicioProductos.php
@@ -5,13 +5,18 @@ Esta página contiene la vista respectiva de Usuarios en donde se encotrará las
 <?php
 // Inicio de sesión e inclusión de rutas para acceder a los datos
 session_start();
-include_once '../../Model/Producto//Producto.php';
-include_once '../../Model/Producto//ProductosModel.php';
+include_once '../../Model/Producto/Producto.php';
+include_once '../../Model/Producto/ProductosModel.php';
+
+
+// Creamos la variable para el llamado de los métodos de la tabla Tipo Usuario y Usuario
+//$tiposUsuarioModel = new TiposUsuarioModel();
+//$usuariosModel = new UsuariosModel(); 
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Productos</title>
+        <title>Usuarios</title>
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">				
 
         <!--Importación de Bootstrap al proyecto-->
@@ -43,8 +48,8 @@ include_once '../../Model/Producto//ProductosModel.php';
                 <div class="col-md-12" style="padding-top: 5px">
                     <!--La class nav nav-pills nos permite hacer menús-->
                     <ul class="nav nav-pills">
-                        <li role="presentation"><a href="../../Controller/controller.php?opcion1=producto&opcion2=listar_productos"><h4>MOSTRAR TODOS</h4></a></li>
-                        <li role="presentation"><a href="#nuevoPROD" data-toggle="modal"><h4>NUEVO PRODUCTO</h4></a></li>
+                        <li role="presentation"><a href="../../Controller/controller.php?opcion1=productoo&opcion2=listar_productos"><h4>MOSTRAR TODOS</h4></a></li>
+                        <li role="presentation"><a href="#nuevoPRO" data-toggle="modal"><h4>NUEVO PRODUCTO</h4></a></li>
                     </ul>
                 </div>
             </div>
@@ -56,36 +61,40 @@ include_once '../../Model/Producto//ProductosModel.php';
                         <div class="panel-body">
                             <div class="col-lg-12">
                                 <div class="table-responsive">
-                                    <!-- Tabla en la que se listaras los usuarios de la Base de Datos -->
+                                    <!-- Tabla en la que se listaras los productos de la Base de Datos -->
                                     <table class="table table-striped table-bordered table-condensed table-hover">
                                         <thead>
                                         <th>ID PRODUCTO</th>
                                         <th>NOMBRE PRODUCTO</th>
-                                        <th>DESCRIPCION PRODUCTO</th>
-                                        <th>GRAVA IVA O NO EL PRODUCTO</th>
+                                        <th>DESCRIPCION DEL PRODUCTO</th>
+                                        <th>GRABA_IVA_O_NO</th>
                                         <th>COSTO PRODUCTO</th>
-                                        <th>PVP PRODUCTO </th>
+                                        <th>PVP PRODUCTO</th>
                                         <th>ESTADO PRODUCTO</th>
                                         <th>STOCK PRODUCTO</th>
+                                        
                                         <th colspan="2">ACCIONES</th>
                                         </thead>
                                         <?php
-                                        // Verificamos si existe la variable de sesión que contiene la lista de productos
+                                        // Verificamos si existe la variable de sesión que contiene la lista de Productos
                                         if (isset($_SESSION['listadoProductos'])) {
-                                            // Deserializamos y mostraremos los atributos de los productos usando un ciclo for
+                                            // Deserializamos y mostraremos los atributos de los usuarios usando un ciclo for
                                             $listado = unserialize($_SESSION['listadoProductos']);
-                                            foreach ($listado as $prod) {
+                                            foreach ($listado as $pro) {
+                                                // Obtenemos datos de tipo usuario de un usuario en específico
+//                                                $tipoUsuario = $tiposUsuarioModel->getTipoUsuario($usu->getID_TIPO_USU());
+//                                                $estado = $usuariosModel->obtenerEstadoUsuario($usu->getID_USU());
                                                 ?>
                                                 <tr>
-                                                    <td><?php echo $prod->getID_PROD(); ?></td>
-                                                    <td><?php echo $prod->getNOMBRE_PROD(); ?></td>
-                                                    <td><?php echo $prod->getDESCRIPCION_PROD(); ?></td>
-                                                    <td><?php echo $prod->getGRABA_IVA_PROD(); ?></td>
-                                                    <td><?php echo $prod->getCOSTO_PROD(); ?></td>
-                                                    <td><?php echo $prod->getPVP_PROD(); ?></td>
-                                                    <td><?php echo $prod->getESTADO_PROD(); ?></td>
-                                                    <td><?php echo $prod->getSTOCK_PROD(); ?></td>
-                                                   
+                                                    <td><?php echo $pro->getID_PROD(); ?></td>
+                                                    <td><?php echo $pro->getNOMBRE_PROD(); ?></td>
+                                                    <td><?php echo $pro->getDESCRIPCION_PROD(); ?></td>
+                                                    <td><?php echo $pro->getGRAVA_IVA_PROD(); ?></td>
+                                                    <td><?php echo $pro->getCOSTO_PROD(); ?></td>
+                                                    <td><?php echo $pro->getPVP_PROD(); ?></td>
+                                                    <td><?php echo $pro->getESTADO_PROD(); ?></td>
+                                                    <td><?php echo $pro->getSTOCK_PROD(); ?></td>
+                                                  
                                                     <td align="center"><a href=""><span class="glyphicon glyphicon-pencil">Editar</span></a></td>
                                                     <td align="center"><a href=""><span class="glyphicon glyphicon-remove">Eliminar</span></a></td>
                                                 </tr>
@@ -102,7 +111,7 @@ include_once '../../Model/Producto//ProductosModel.php';
             </div>
 
             <!--Ventana emergente para Nuevo producto-->
-            <div class="modal fade" id="nuevoPROD">
+            <div class="modal fade" id="nuevoPRO">
                 <div class="modal-dialog">
                     <form class="form-horizontal" action="#ventanasEmergentes">
                         <div class="modal-content">
@@ -118,36 +127,70 @@ include_once '../../Model/Producto//ProductosModel.php';
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <div class="col-md-3 col-md-offset-1">
-                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> Nombre Productos</label>
+                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> ID PRODUCTO</label>
                                             </div>
                                             <div class="col-md-7">
-                                                <input type="text" class="form-control" placeholder="Ingrese los Productos" required />
+                                                <input type="text" class="form-control" placeholder="Ingrese ID del producto" required />
                                             </div>
                                         </div>
                                         <div class="form-group">
                                             <div class="col-md-3 col-md-offset-1">
-                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> DESCRIPCION</label>
+                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> Nombre Producto</label>
                                             </div>
                                             <div class="col-md-7">
-                                                <input type="text" class="form-control" placeholder="Ingrese la descripcion" required />
+                                                <input type="text" class="form-control" placeholder="Ingrese Nombres del producto" required />
                                             </div>
                                         </div>
                                         <div class="form-group">
                                             <div class="col-md-3 col-md-offset-1">
-                                                <label class="control-label">Graba iva o no</label>
+                                                <label class="control-label"> &nbsp;&nbsp;&nbsp;&nbsp;Descripcion</label>
                                             </div>
                                             <div class="col-md-7">
-                                                <input type="text" class="form-control" placeholder="S o N" />
+                                                <input type="text" class="form-control" placeholder="Ingrese la descripcion" />
                                             </div>
                                         </div>
                                         <div class="form-group">
                                             <div class="col-md-3 col-md-offset-1">
-                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> Costo</label>
+                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> Graba iva o no</label>
                                             </div>
                                             <div class="col-md-7">
-                                                <input type="text" class="form-control" placeholder="Costo del Producto" required />
+                                                <input type="text" class="form-control" placeholder="S o N" required />
                                             </div>
                                         </div>
+                                        <!--nuevo-->
+                                        <div class="form-group">
+                                            <div class="col-md-3 col-md-offset-1">
+                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> &nbsp;&nbsp;&nbsp;&nbsp;Costo</label>
+                                            </div>
+                                            <div class="col-md-7">
+                                                <input type="text" class="form-control" placeholder="Ingrese el costo del producto" required />
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-md-3 col-md-offset-1">
+                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> &nbsp;&nbsp;&nbsp;&nbsp;PVP</label>
+                                            </div>
+                                            <div class="col-md-7">
+                                                <input type="text" class="form-control" placeholder="Ingrese PVP" required />
+                                            </div>
+                                        </div>
+                                         <div class="form-group">
+                                            <div class="col-md-3 col-md-offset-1">
+                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> &nbsp;&nbsp;&nbsp;&nbsp;Estado</label>
+                                            </div>
+                                            <div class="col-md-7">
+                                                <input type="text" class="form-control" placeholder="A o I" required />
+                                            </div>
+                                        </div>
+                                         <div class="form-group">
+                                            <div class="col-md-3 col-md-offset-1">
+                                                <label class="control-label"><span class="glyphicon glyphicon-asterisk"></span> &nbsp;&nbsp;&nbsp;&nbsp;Stock</label>
+                                            </div>
+                                            <div class="col-md-7">
+                                                <input type="text" class="form-control" placeholder="Stock" required />
+                                            </div>
+                                        </div>
+                                        <!--nuevo-->
                                     </div>
                                 </div>
                             </div>
@@ -164,4 +207,3 @@ include_once '../../Model/Producto//ProductosModel.php';
         </div>
     </body>
 </html>
-
